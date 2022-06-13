968c638566f901d995b047ee5340bc633669f877
BertKiv <56419191+BertKiv@users.noreply.github.com>
2022-06-14 00:31:06 +0200

    Update .bashrc.sh

--- conf/.bashrc.sh
+++ conf/.bashrc.sh
@@ -5,7 +5,7 @@ then
     touch /var/run/mysqld/gitpod-init.lock
 
     # initialize database structures on disk, if needed
-    [ ! -d /workspace/.mysql ] && mysqld --initialize-insecure --user=gitpod
+    [ ! -d /workspace/.mysql ] && mysqld --initialize-insecure
 
     # launch database, if not running
     [ ! -e /var/run/mysqld/mysqld.pid ] && mysqld --daemonize
